8bc8804bac810506185c432b8fd91483471b4a2c
b2dZ <102845207+b2dZ@users.noreply.github.com>
2022-04-01 23:18:07 -0600

    added countdown.  removed. excess variables

--- main.ts
+++ main.ts
@@ -4,8 +4,37 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     if (timerON) {
-        elapsedTime = input.runningTime() - buttonTime
-        if (elapsedTime > 5000) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            # . . # .
+            # . # . .
+            . # . . .
+            `)
+        timerON = false
+    }
+})
+let buttonTime = 0
+let timerON = false
+timerON = false
+basic.showNumber(5)
+let buttonDelay = 5000
+basic.forever(function () {
+    if (timerON) {
+        if (input.runningTime() - buttonTime > 1000) {
+            basic.showNumber(4)
+        }
+        if (input.runningTime() - buttonTime > 2000) {
+            basic.showNumber(3)
+        }
+        if (input.runningTime() - buttonTime > 3000) {
+            basic.showNumber(2)
+        }
+        if (input.runningTime() - buttonTime > 4000) {
+            basic.showNumber(1)
+        }
+        if (input.runningTime() - buttonTime > 5000) {
+            timerON = false
             basic.showLeds(`
                 # . . . #
                 . # . # .
@@ -13,25 +42,6 @@ input.onButtonPressed(Button.B, function () {
                 . # . # .
                 # . . . #
                 `)
-        } else {
-            basic.showLeds(`
-                . . . . #
-                . . . . #
-                # . . # .
-                # . # . .
-                . # . . .
-                `)
         }
-    }
-})
-let buttonTime = 0
-let elapsedTime = 0
-let timerON = false
-timerON = false
-basic.showNumber(5)
-elapsedTime = 0
-basic.forever(function () {
-    if (timerON) {
-    	
     }
 })
